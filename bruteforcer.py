2e422a0436a6167b7179adddd98465bc09ca5327
orkhan-alibayli <99633184+orkhan-alibayli@users.noreply.github.com>
2022-12-05 11:24:48 +0400

    Create bruteforcer.py

---
+++ bruteforcer.py
@@ -0,0 +1,26 @@
+#Simple http bruteforcer
+
+import requests
+from requests.structures import CaseInsensitiveDict
+
+class Bruteforcer:
+
+    def __init__(self, url, wordlist, timeout) -> None:
+        self.session = requests.Session()
+        self.headers = CaseInsensitiveDict()
+        self.headers = {'Connection': 'keep-alive', 'Keep-Alive': 'timeout=5, max=100'}
+        self.url = url
+        self.wordlist = wordlist
+        self.timeout = timeout
+
+    
+    def run_bruteforcer(self):
+        '''opens given wordlist file and send requests for each line in same TCP session'''
+        
+        with open(self.wordlist, 'r') as file:
+            for directory in file:
+                directory = directory.replace('\n','')
+                directory = self.url + directory
+                response = self.session.get(directory, headers=self.headers,timeout=self.timeout)
+                if(response.status_code == 200):
+                    print('directory found --->', directory)
